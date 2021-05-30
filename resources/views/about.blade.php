a9cfcb5ae9b30380cb54ce6c4d64d982a0348ea5
nannamaryb <nannamarybernabas1234@gmail.com>
2021-05-30 23:26:42 +0530

    herbs-app

---
+++ resources/views/about.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/register">REGISTER</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/herbs">HERBS</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+        <br>
+      <div class="container">
+        <br>
+      <div class="row">
+      <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
+          <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img height="400" width="400" src="/images/h1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img height="400" width="400" src="/images/h3.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img height="400" width="400" src="/images/h4.jpg" class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+
+              About 8,000 herbal remedies have been codified in AYUSH systems in INDIA. Ayurveda, Unani, Siddha and Folk (tribal) medicines are the major systems 
+              of indigenous medicines. Among these systems, Ayurveda and Unani Medicine are most developed and widely practised in India.
+      
+            </div>
+          </div>
+      <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
+          <p> 
+            <h5><strong>HERBS</strong></h5>
+              The word “herb” has been derived from the Latin word, “herba” and an old French word “herbe”.
+               Now a days, herb refers to any part of the plant like fruit, seed, stem, bark, flower, leaf, stigma or a root, 
+               as well as a non-woody plant. Earlier, the term “herb” was only applied to non-woody plants, including those that 
+               come from trees and shrubs. These medicinal plants are also used as food, flavonoid, medicine or perfume and also in 
+               certain spiritual activities.
+          </p>
+          <p>
+            Plants have been used for medicinal purposes long before prehistoric period. Ancient Unani manuscripts Egyptian papyrus and
+            Chinese writings described the use of herbs.  Evidence exist that Unani Hakims, Indian Vaids and European and Mediterranean 
+            cultures were using herbs for over 4000 years as medicine. Indigenous cultures such as Rome, Egypt, Iran, Africa and America 
+            used herbs in their healing rituals, while other developed traditional medical systems such as Unani, Ayurveda and Chinese Medicine 
+            in which herbal therapies were used systematically.            
+          <p>
+            Traditional systems of medicine continue to be widely practised on many accounts. Population rise, inadequate supply of drugs,
+            prohibitive cost of treatments, side effects of several synthetic drugs and development of resistance to currently used drugs
+            for infectious diseases have led to increased emphasis on the use of plant materials as a source of medicines for a wide variety 
+            of human ailments.Among ancient civilisations, India has been known to be rich repository of medicinal plants. The forest in India
+            is the principal repository of large number of medicinal and aromatic plants, which are largely collected as raw materials for manufacture 
+            of drugs and perfumery products.    
+          </p>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col col-12 col-sm-4 col-md-4 col-lg-4">
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/herbs.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/register">REGISTER</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/herbs">HERBS</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+      <br>
+      <br>
+      <div class="container">
+        <div class="row">
+          <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="card" style="width: 18rem; height: 18rem;">
+              <img src="/images\h2.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <strong>PARSLEY</strong>
+               <br>
+               <p class="card-text"><button  class="btn btn-success btn-sm" >BUY NOW</button></p>
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="card" style="width: 18rem;height: 18rem; ">
+              <img src="/images/mint.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <strong>MINT</strong>
+                <br>
+                <p class="card-text"><button  class="btn btn-success btn-sm" >BUY NOW</button></p>   
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="card" style="width: 18rem; height: 18rem;">
+              <img src="/images/rosemary.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <strong>ROSEMARY</strong>
+                <br>
+                <p class="card-text"><button  class="btn btn-success btn-sm" >BUY NOW</button></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      <br>
+      <br>
+        <div class="row">
+          <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="card" style="width: 18rem; height: 18rem;">
+              <img src="/images/coriander.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <strong>CORIANDER</strong>
+                <br>
+                <p class="card-text"><button  class="btn btn-success btn-sm" >BUY NOW</button></p>
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="card" style="width: 18rem;height: 18rem; ">
+              <img src="/images/dill.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <strong>DILL</strong>
+                <br>
+                <p class="card-text"><button  class="btn btn-success btn-sm" >BUY NOW</button></p>
+              </div>
+            </div>
+          </div>
+          <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+            <div class="card" style="width: 18rem; height: 18rem;">
+              <img src="/images/basil.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <strong>BASIL</strong>
+                <br>
+                <p class="card-text"><button  class="btn btn-success btn-sm" >BUY NOW</button></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>  
+</body>
+</html>
